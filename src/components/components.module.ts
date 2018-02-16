604808b69ffbef38cf60d95fa813d05bd47df562
Adrian Adams <adrianadams@Adrians-MacBook-Pro.local>
2018-02-16 10:05:12 -0600

    Fix

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ExpandableComponent } from './expandable/expandable';
-@NgModule({
-	declarations: [ExpandableComponent],
-	imports: [],
-	exports: [ExpandableComponent]
-})
-export class ComponentsModule {}
