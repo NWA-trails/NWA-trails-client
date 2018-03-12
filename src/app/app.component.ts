53588a6b3e1a8cf0366cf70649b7a0843dad27ab
mig <mhinkley42@gmail.com>
2018-03-12 10:40:55 -0500

    trying http for geojson, broken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AndroidPermissions } from '@ionic-native/android-permissions';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
