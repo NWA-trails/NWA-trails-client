b98bcbd0cce049531945baf0e1180639a60326b4
mig <mhinkley42@gmail.com>
2018-04-05 18:17:14 -0500

    added condition report to set up front end for sending a report to server

--- src/pages/condition/condition.ts
+++ src/pages/condition/condition.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera } from '@ionic-native/camera';
 import { AlertController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Geolocation } from '@ionic-native/geolocation';
+import { conditionDetails } from './conditionDetails'
 
 @Component({
   selector: 'page-condition',
@@ -9,7 +12,9 @@ import { AlertController } from 'ionic-angular';
 })
 export class ConditionPage {
   public picture: string;
-  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public camera: Camera) {
+  public pictureData: string;
+  public checkPic: string;
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController, public camera: Camera, public http: HttpClient, public geolocation : Geolocation) {
 
   }
 
@@ -21,6 +26,7 @@ export class ConditionPage {
       targetHeight: 1000
     }).then((imageData) => {
       this.picture = "data:image/jpeg;base64," + imageData;
+      this.pictureData = imageData;
     }, (err) => {
       alert("prob");
       alert(err);
@@ -38,8 +44,49 @@ export class ConditionPage {
 
   submit()
   {
-    alert("Thank you for submitting a conditions report!");
-//re-run the view load function if the page has one declared
-    this.navCtrl.push(ConditionPage);
+     this.geolocation.getCurrentPosition().then((position) => {
+      var repot: conditionDetails = {
+        //waiting to get user storage sorted out
+        username:"test",
+        //still have to add in the html
+        description:"there is a condition to report and this is a test:)",
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        image: this.pictureData
+      };
+      alert(JSON.stringify(repot));
+    }, (err) => {
+      alert("getting location error");
+      alert(err);
+    });
+
+
+
+
+//     alert("Thank you for submitting a conditions report!");
+//
+// //re-run the view load function if the page has one declared
+//     //this.navCtrl.push(ConditionPage);
+//     this.http.post('https://nwa-trails-webservice.herokuapp.com/user/image' , this.pictureData).subscribe( res => {
+//
+//     this.checkPic = "data:image/jpeg;base64," + res["image"];
+//     if(this.checkPic != this.picture)
+//       alert("images not the same");
+//      else alert("all good with the pics");
+//
+//    }, (err) => {
+//      alert("error with post");
+//      alert(JSON.stringify(err));
+//    });
+  }
+
+  stringToByteArray(s)
+  {
+
+    var data = [];
+    for (var i = 0; i < s.length; i++){
+      data.push(s.charCodeAt(i));
+    }
+    return data;
   }
 }
---
+++ src/pages/condition/conditionDetails.ts
@@ -0,0 +1,7 @@
+export interface conditionDetails {
+    username: string;
+    image: any;
+    description: string;
+    lat: number;
+    lng: number;
+}
